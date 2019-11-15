feac1df84409163d98f6067c91c40e874e73da6f
Lou <lou.d.chaney@gmail.com>
2019-11-15 16:15:58 -0500

    logic done!!! ��‍������

--- script.js
+++ script.js
@@ -2,17 +2,28 @@
 
 const puzzle = {};
 
-// 2. Create array of 4 image tags for different pieces of an image 
-//this is the answer key in order by index number
+//Create array of images for different pieces of a puzzle 
+//this is the answer key
 
 puzzle.pieces = [
-    "<img src='assets/plantImage01.jpg' alt='1' title='1'>",
-    "<img src='assets/plantImage02.jpg' alt='2' title='2'>",
-    "<img src='assets/plantImage03.jpg' alt='3' title='3'>",
-    "<img src='assets/plantImage04.jpg' alt='4' title='4'>"
+    {url: "<img src='assets/plantImage01.jpg' alt='1' title='1'>",
+    answer: 1,
+    currentPosition: ""}
+    ,
+    { url: "<img src='assets/plantImage02.jpg' alt='2' title='2'>",
+    answer: 2,
+    currentPosition: ""}
+    ,
+    {url: "<img src='assets/plantImage03.jpg' alt='3' title='3'>",
+    answer: 3,
+    currentPosition: ""}
+    ,
+    {url: "<img src='assets/plantImage04.jpg' alt='4' title='4'>",
+    answer: 4,
+    currentPosition: ""}
 ];
 
-//3. create function to get random numberimage
+//function to randomize the array order
 //this creates a copy of the original array, but in random order
 
 //  Durstenfeld shuffle from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
@@ -31,44 +42,27 @@ puzzle.randomizer = function(array) {
 
 puzzle.placeImages = function(array) {
     for (let i = array.length -1; i >= 0; i--) {
-        $('.puzzleContainer.' + (i + 1) + ' button').html(array[i]);
-        // $('.puzzleContainer.' + (i + 1) + ' button').attr('id', $('img').attr('title'));
+        $('.puzzleContainer.' + (i + 1) + ' button').html(array[i].url);
+        array[i].currentPosition = i+1;
     }
 }
 
 
-
-
 //4. check if random array matches the correct answer
-
+puzzle.score = 0;
 puzzle.status = function() {
     //checks if the two arrays match
-    if (puzzle.pieces === puzzle.randomArray) {
-        console.log('the puzzle pieces are in the right spot')
-
-        //tell them they've won
+    // each piece in correct spot is worth 1 point
+    puzzle.randomArray.forEach(function(element) {
+        if (element.answer === element.currentPosition) {
+             puzzle.score++;
+        }
+    })
+    //if score equals array length, tell player they've won, if not, clear the score for the next turn
+    
+    if (puzzle.score === puzzle.randomArray.length) {
         $('h2.win').html("You've Won!!!");
-
-    } else console.log('puzzle pieces are out of order');
-    
-    // puzzle.pieces.forEach(element => {
-    //     console.log(element);
-    //     // if (element === puzzle.randomArray) {
-    //     //     console.log('it works');
-    //     // } else console.log('nope')      
-    // });
-    
-    // for (i = 0; i < $('.gameBoard > div > button.each').length; i++) {
-    //     console.log($('.gameBoard > div > button.each').attr('class'));
-    // }
-
-    // $('button').each(function() {
-    //     // console.log($(this).attr('class'));
-    // })
-
-    // $('button').each(function() {
-    //     console.log($(this > 'img').attr('class'));
-    // })
+    } else (puzzle.score = 0);
 }
 
 //6. If user clicks on img store index#(class#) to let 'active' add class of active
@@ -78,73 +72,42 @@ puzzle.active = "";
 puzzle.secondChoice = "";
 
 puzzle.selectPiece = function() {
+    //7. when an image is clicked, if 'active' does not equal nothing, store value of img to let '2ndimg' add class of '2ndimg'
     $('.puzzleContainer button').on('click', function() {
         if ($('button.active').length !== 0 && $(this).attr('class') !== 'active') {
-            // $(this).removeClass();
-            // $('button').removeClass();
-            //add class of secondChoic
+
+            //add class of secondChoice
             $(this).addClass('secondChoice');
-            puzzle.secondChoice = $(this)[0].firstChild;
-            console.log(puzzle.secondChoice);
+            puzzle.secondChoice = parseInt($(this)[0].value);           
 
-            //find array index of selection
+            //clear active class on button
+            $(puzzle.activeButton).removeClass('active');
 
-            puzzle.secondChoiceLocation = $.inArray(`"${puzzle.secondChoice}"`, puzzle.randomArray);
-            console.log(`second choice is at ${puzzle.secondChoiceLocation}`)
-
-            //clear active and secondChoice class
-            $(puzzle.active).removeClass('active');
-            $(puzzle.secondChoice).removeClass('secondChoice');
-
-            //fin
             //swap position of two selected items in array
-            // a = parseInt(puzzle.active.firstChild.title) - 1;
-            // b = parseInt(puzzle.secondChoice.firstChild.title) - 1;
-
+            a = puzzle.active -1;
+            b = puzzle.secondChoice -1;
             
-            // [puzzle.randomArray[a], puzzle.randomArray[b]] = [puzzle.randomArray[b], puzzle.randomArray[a]];
+            [puzzle.randomArray[a], puzzle.randomArray[b]] = [puzzle.randomArray[b], puzzle.randomArray[a]];
 
             //move puzzle pieces to new spots
             puzzle.placeImages(puzzle.randomArray);
             //check if puzzle is solved
             puzzle.status();
-
-            console.log(puzzle.randomArray);
-            
-
         }
-        // else if {
 
-        // }
         else {
             // $('button').removeClass();
             $(this).addClass('active');
-            puzzle.active = $(this)[0].firstChild;
-            console.log(puzzle.active);
+            puzzle.activeButton = $(this);
+            puzzle.active = parseInt($(this)[0].value);
         }
     } )
 }
-
-//7. when an image is clicked, if 'active' does not equal nothing, store value of img to let '2ndimg' add class of '2ndimg'
-
-
-
-//8. then put value of active replace 2nd and 2nd will replace active, then remove active and 2nd classes
-
-//3. run function to check if div id matches image index #
-
-//4. if correct, image is completed, player wins
-
-//display "You've won" over the game grid
-//if not correct, nothing happens
-
-//7. assign active class to change border styling 
 
 puzzle.init = function () {
     puzzle.randomArray = puzzle.pieces.slice(0);
     puzzle.randomizer(puzzle.randomArray);
     puzzle.placeImages(puzzle.randomArray);
-    console.log(puzzle.randomArray);
     puzzle.status();
     puzzle.selectPiece();
 }
@@ -154,8 +117,8 @@ $(function () {
 });
 
 //accessibility-------
-// tab between the 2 moveable game pieces
-// space key to move them
+// tab through the game pieces
+// space key to select them
 
 //stretch goals-------------------------
 // add a timer
@@ -205,3 +168,4 @@ $(function () {
 // reset button
 // multiple images
 //larger grid
+
